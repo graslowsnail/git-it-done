3c0cc23b148b7b82f0d66aa65d6bfd5184b90a59
graslowsnail <graslowsnail@gmail.,com>
2022-09-17 14:15:17 -0500

    added limit warning checked Header to see if there was more imformation

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +10,11 @@ var getRepoIssues = function (repo) {
             response.json().then(function (data) {
                 //pass response data to dom function
                 displayIssues(data);
+
+                //check if api had pignated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -52,4 +58,17 @@ var displayIssues = function (issues) {
     }
 };
 
-getRepoIssues("graslowsnail/prework-study-guide");
+var displayWarning = function (repo) {
+    //add text to wearning container
+    limitWarningEl.textContent = "To see more than 30 isues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warining contrainer
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
