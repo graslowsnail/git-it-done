35f0eb739ae09391c39d7c8a4e2a0a3e23618281
graslowsnail <graslowsnail@gmail.,com>
2022-09-17 13:48:14 -0500

    added elements of isues onto the page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,5 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -5,7 +7,8 @@ var getRepoIssues = function (repo) {
         //request was sucsessful
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data);
+                //pass response data to dom function
+                displayIssues(data);
             });
         }
         else {
@@ -14,4 +17,39 @@ var getRepoIssues = function (repo) {
     });
 }
 
-getRepoIssues("facebook/react");
+var displayIssues = function (issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no opoen isues!";
+        return;
+
+    }
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+        // create span to; hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        //append to container
+        issueEl.appendChild(titleEl);
+
+        //create a type element
+        var typeEl = document.createElement("span");
+
+        //check it issuse is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+        //append to container 
+        issueEl.appendChild(typeEl);
+
+        issueContainerEl.appendChild(issueEl);
+    }
+};
+
+getRepoIssues("graslowsnail/prework-study-guide");
